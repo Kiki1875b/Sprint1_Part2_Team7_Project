6cbaec256436ee54dc5cc0da1da2472fab71e850
Kiki1875b <kevinheo0413@gmail.com>
2025-03-13 11:11:11 +0900

    init : project init

---
+++ src/main/java/team7/hrbank/HrbankApplication.java
@@ -0,0 +1,13 @@
+package team7.hrbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrbankApplication.class, args);
+	}
+
+}
