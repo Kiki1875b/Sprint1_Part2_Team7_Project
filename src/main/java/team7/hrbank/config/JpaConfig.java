4b1643f524d0ee9f24a740f9e6b4c1de0cd2be84
hyanyul <tjdwl@gmail.com>
2025-03-13 18:10:25 +0900

    feat : Employee 엔티티, EmployeeStatus enum, JpaConfig 추가

---
+++ src/main/java/team7/hrbank/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package team7.hrbank.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
